f61d4e7cd55d9b06242e3c1c2d84f3c210b2a61b
Marquis <smcneal89@gmail.com>
2022-09-07 10:38:58 -0400

    added calendar icon

--- src/componets/Header/CTA.js
+++ src/componets/Header/CTA.js
@@ -4,7 +4,7 @@
 function CTA() {
   return (
     <div className='cta'>
-      <a href="https://drive.google.com/file/d/1UyeT-dYxEBQqu9qWycHxbJe4_sgKk3-w/view?usp=sharing" download className='btn'> Resume</a>
+      <a href="https://drive.google.com/file/d/1UyeT-dYxEBQqu9qWycHxbJe4_sgKk3-w/view?usp=sharing" download className='btn'> Résumé</a>
       <a href="/contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
--- src/componets/Header/Socials.js
+++ src/componets/Header/Socials.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import {AiFillInstagram} from 'react-icons/ai'
 import {FaLinkedinIn} from 'react-icons/fa'
+import {BsCalendar3} from 'react-icons/bs'
 
 function Socials() {
   return (
     <div className="socials">
-      <a href="" target={'_blank'}><FaLinkedinIn/></a>
+      <a href="" target={'_blank'}> <BsCalendar3/></a>
       <a href="" target={'_blank'}><AiFillInstagram/></a>
-      <a href="" target={'_blank'}>
-        </a>
+      <a href="" target={'_blank'}><FaLinkedinIn/></a>
+      
     </div>
   )
 }
